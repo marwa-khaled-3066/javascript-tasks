92ccf355975029cd1a48fc5762f3a10e8f6c5b3c
marwa-khaled-3066 <mksaleh96@gmail.com>
2025-09-10 22:16:20 +0300

    first commit

---
+++ Task1/myjavascript.js
@@ -0,0 +1,7 @@
+const student = {
+    name: "Rana",
+    age: 18,
+    grade: 12
+}
+let message = "The student " + student.name + " is " + student.age + " years old and in grade " + student.grade;
+console.log(message);
