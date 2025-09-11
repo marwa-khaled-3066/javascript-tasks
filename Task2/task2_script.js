92bab8f71e84dd6785b69b222eafa02e4be2e1a0
marwa-khaled-3066 <mksaleh96@gmail.com>
2025-09-11 19:37:32 +0300

    Task 2 added

--- Task2/task2_script.js
+++ Task2/task2_script.js
@@ -1,6 +1,37 @@
 let name = "Marwa";
 let age = 28;
-let student= false;
+let isStudent= false;
 
 var x = 20;
-var y = 50;
+var y = 5;
+document.write(
+    "x= " + x + "<br>" + "y= " + y + "<br>"
+)
+document.write("Sum: x+y= " + (x+y) + "<br>");
+document.write("Difference: x-y= " + (x-y) + "<br>");
+document.write("Product: x*y= " + (x*y) + "<br>");
+document.write("Quotient: x/y= " + (x/y) + "<br>");
+document.write("Remainder: x%= " + (x%y) + "<br>");
+
+let fruits = ["apple", "orange", "banana", "mango"];
+document.write("First fruit: " + fruits[0] + "<br>");
+fruits.push("pineapple");
+fruits.shift();
+document.write("Removed first fruit:" + fruits + "<br>");
+
+const student = {
+    name: "Ahmed",
+    grade: "B",
+    subjects: ["Math", "Physics", "Chemistry"],
+};
+document.write("Student name: " + student.name +"<br>");
+document.write("Student grade: " + student.grade +"<br>");
+
+// Create an Array from the Properties
+const myArray = Object.values(student);
+// Stringify the Array
+let text = myArray.toString();
+document.write(text);
+
+Object.defineProperty(student, "isGraduated", {value: false});
+document.write("Student graduatation status: " + student.isGraduated +"<br>");
